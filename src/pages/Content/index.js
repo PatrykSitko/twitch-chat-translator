9e0b4c4bb198e53b9e4ecafa120b60a36de75b66
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-11-13 18:53:25 +0100

    working translation

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -6,23 +6,15 @@ const isOnTwitchPage = window.location.hostname.includes('twitch');
 window.onload = () => {
   if (isOnTwitchPage) {
     setInterval(async () => {
-      const chatMessages = document.getElementsByClassName(
-        'chat-line__message'
-      );
+      const chatMessages = document.getElementsByClassName('text-fragment');
       for (let chatMessage of chatMessages) {
         if (!chatMessage.getAttribute('translated')) {
-          const innerText = chatMessage.innerText;
-          const senderName = innerText.substring(0, innerText.indexOf(':') + 1);
-          const senderMessage = innerText.substring(
-            innerText.indexOf(':'),
-            innerText.length
-          );
           const { translation: translatedSenderMessage } = await translate(
             'korean',
             'polish',
-            senderMessage
+            chatMessage.innerText
           );
-          chatMessage.innerText = senderName + ': ' + translatedSenderMessage;
+          chatMessage.innerText = translatedSenderMessage;
           chatMessage.setAttribute('translated', 'true');
         }
       }
